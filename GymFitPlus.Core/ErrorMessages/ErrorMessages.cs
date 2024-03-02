dcf6a9ffb10fa9931438c1736962e2921b1cde42
Valentin Kirilov <valkilmaar@gmail.com>
2024-03-03 01:05:48 +0200

    Add DateTimeModelBinder

--- GymFitPlus.Core/ErrorMessages/ErrorMessages.cs
+++ GymFitPlus.Core/ErrorMessages/ErrorMessages.cs
@@ -1,10 +1,18 @@
-﻿namespace GymFitPlus.Core.ErrorMessages
+﻿using GymFitPlus.Infrastructure.Constants;
+
+namespace GymFitPlus.Core.ErrorMessages
 {
     public static class ErrorMessages
     {
         public const string RequiredErrorMessage = "The field {0} is required!";
+
         public const string LengthErrorMessage = "The field {0} must be between {2} and {1} characters long!";
+
         public const string EmailErrorMessage = "Invalid email adress!";
+
         public const string ConfirmPassErrorMessage = "The password and confirmation password do not match.";
+
+        public const string DateFormatErrorMessage = 
+            $"The date format is incorect! Allow format is : {DataConstants.ApplicationUserConstants.DateFormat}";
     }
 }
