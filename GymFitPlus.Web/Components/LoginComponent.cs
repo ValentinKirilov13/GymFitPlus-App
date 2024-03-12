86256b137f257d858f802df4d9ec20e0174fdb5c
Valentin Kirilov <valkilmaar@gmail.com>
2024-03-12 19:02:06 +0200

    Add ViewCcomponents for register and login page

---
+++ GymFitPlus.Web/Components/LoginComponent.cs
@@ -0,0 +1,15 @@
+﻿using GymFitPlus.Core.ViewModels.AccountViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymFitPlus.Web.Components
+{
+    public class LoginComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = new LoginViewModel();
+
+            return await Task.FromResult<IViewComponentResult>(View(model));
+        }
+    }
+}
