e6934879f9cc07da732357555a7516699a362ac6
Valentin Kirilov <valkilmaar@gmail.com>
2024-03-17 00:10:56 +0200

    Add ExerciseController and Component

---
+++ GymFitPlus.Web/Components/SearchExerciseComponent.cs
@@ -0,0 +1,13 @@
+﻿using GymFitPlus.Core.ViewModels.ExerciseViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymFitPlus.Web.Components
+{
+    public class SearchExerciseComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(AllExercisesQueryModel model)
+        {
+            return await Task.FromResult<IViewComponentResult>(View(model));
+        }
+    }
+}
---
+++ GymFitPlus.Web/Controllers/ExerciseController.cs
@@ -0,0 +1,108 @@
+﻿using GymFitPlus.Core.Contracts;
+using GymFitPlus.Core.ViewModels.ExerciseViewModels;
+using Microsoft.AspNetCore.Mvc;
+using static GymFitPlus.Core.ErrorMessages.ErrorMessages;
+
+namespace GymFitPlus.Web.Controllers
+{
+    public class ExerciseController : BaseController
+    {
+        private readonly IExerciseService _exerciseService;
+        private readonly ILogger<ExerciseController> _logger;
+
+        public ExerciseController(IExerciseService exerciseService, ILogger<ExerciseController> logger)
+        {
+            _exerciseService = exerciseService;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index([FromQuery]AllExercisesQueryModel query)
+        {
+            IEnumerable<ExerciseAllViewModel> model = await _exerciseService.AllExerciseAsync(/*query*/);
+
+            ViewBag.Query = query;
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            ExerciseDetailViewModel model = await _exerciseService.FindExerciseByIdAsync(id);
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult AddExercise()
+        {
+            ExerciseDetailViewModel model = new ExerciseDetailViewModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddExercise(ExerciseDetailViewModel viewModel)
+        {
+            if (viewModel.MuscleGroup == default)
+            {
+                ModelState.AddModelError(nameof(viewModel.MuscleGroup),string.Format(RequiredErrorMessage,"Muscle group"));
+            }
+          
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+
+            await _exerciseService.AddExerciseAsync(viewModel);
+
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditExercise(int id)
+        {
+            ExerciseDetailViewModel model = await _exerciseService.FindExerciseByIdAsync(id);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditExercise(int id, ExerciseDetailViewModel viewModel)
+        {
+            if (id != viewModel.Id)
+            {
+                return BadRequest();
+            }
+
+            if (viewModel.MuscleGroup == default)
+            {
+                ModelState.AddModelError(nameof(viewModel.MuscleGroup), string.Format(RequiredErrorMessage, "Muscle group"));
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+
+            await _exerciseService.EditExerciseAsync(viewModel);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteExercise(int id, ExerciseDetailViewModel viewModel)
+        {
+            if (id != viewModel.Id)
+            {
+                return BadRequest();
+            }
+
+            await _exerciseService.DeleteExerciseAsync(viewModel.Id);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
