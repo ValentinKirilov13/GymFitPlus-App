9453979e2c414daade27afca474bfac6baa2b32b
Valentin Kirilov <valkilmaar@gmail.com>
2024-03-17 20:30:31 +0200

    Add static files for index view of exercise  controller

---
+++ GymFitPlus.Web/wwwroot/js/IndexExercises.js
@@ -0,0 +1,16 @@
+﻿
+function trackElement() {
+    var sourceElement = document.getElementById("sourceElement");
+    var targetElementP = document.getElementById("targetElementP");
+    var targetElementN = document.getElementById("targetElementN");
+    
+    var sourceHeight = sourceElement.offsetHeight;
+    targetElementN.style.height = sourceHeight + "px";  
+    targetElementP.style.height = sourceHeight + "px";
+};
+
+window.onload = function () {
+    trackElement();
+    window.addEventListener('resize', trackElement);
+};
+
