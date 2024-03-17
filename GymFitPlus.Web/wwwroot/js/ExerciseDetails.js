21073715ec9250c1a5b51e843b73298f9d14bfd5
Valentin Kirilov <valkilmaar@gmail.com>
2024-03-18 01:59:58 +0200

    Add static files and edit some

---
+++ GymFitPlus.Web/wwwroot/js/ExerciseDetails.js
@@ -0,0 +1,22 @@
+﻿function loadData(exerciseId) {
+    $.ajax({
+        url: '/FitnessProgram/GetAllProgramsAjax?withoutExerciseId=' + exerciseId,
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+
+            var ul = document.getElementById("myList");
+
+            data.forEach(function (itemText) {
+                var li = document.createElement("li");
+                var a = document.createElement("a");
+                a.textContent = itemText.name;
+                a.href = "/FitnessProgram/AddExerciseToProgram?programId=" + itemText.id + "&exerciseId=" + exerciseId + "&exerciseCount=" + itemText.exerciseCount;
+                a.classList.add("dropdown-item");
+                a.classList.add("text-light");
+                li.appendChild(a);
+                ul.appendChild(li);
+            });
+        },
+    });
+}
