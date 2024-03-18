16380b89e7b9f1ecdd64cc39193a4e8cb6619785
Valentin Kirilov <valkilmaar@gmail.com>
2024-03-18 14:36:18 +0200

    Initial add workout service and contract

---
+++ GymFitPlus.Core/Services/WorkoutService.cs
@@ -0,0 +1,18 @@
+﻿using GymFitPlus.Core.Contracts;
+using GymFitPlus.Infrastructure.Data.Common;
+
+namespace GymFitPlus.Core.Services
+{
+    public class WorkoutService : IWorkoutService
+    {
+        private readonly IRepository _repository;
+
+        public WorkoutService(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+
+
+    }
+}
