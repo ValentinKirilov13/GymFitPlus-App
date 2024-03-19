1d2ff2c1dad3b7ff7ab95265d83b2e96171c423d
Valentin Kirilov <valkilmaar@gmail.com>
2024-03-19 04:28:54 +0200

    Add workouts funcionality server-side

---
+++ GymFitPlus.Core/ViewModels/WorkoutViewModels/WorkoutAllViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace GymFitPlus.Core.ViewModels.WorkoutViewModels
+{
+    public class WorkoutAllViewModel
+    {
+        public int Id { get; set; }
+
+        public Guid UserId { get; set; }
+  
+        public string FitnessProgramName { get; set; } = string.Empty;
+
+        public DateTime Date { get; set; }
+
+        public int Duration { get; set; }
+    }
+}
---
+++ GymFitPlus.Core/ViewModels/WorkoutViewModels/WorkoutDetailViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static GymFitPlus.Infrastructure.Constants.DataConstants.WorkoutConstants;
+
+namespace GymFitPlus.Core.ViewModels.WorkoutViewModels
+{
+    public class WorkoutDetailViewModel : WorkoutAllViewModel
+    {
+        [Required]
+        public int FitnessProgramId { get; set; }
+
+
+        [MaxLength(NoteMaxLenght)]
+        public string? Note { get; set; }
+    }
+}
