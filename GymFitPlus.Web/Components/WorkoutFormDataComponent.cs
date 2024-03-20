b10c39df7f42b2cb1514680cdce027fae4176eb2
Valentin Kirilov <valkilmaar@gmail.com>
2024-03-20 03:21:48 +0200

    Add Workout component

---
+++ GymFitPlus.Web/Components/WorkoutFormDataComponent.cs
@@ -0,0 +1,14 @@
+﻿
+using GymFitPlus.Core.ViewModels.WorkoutViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymFitPlus.Web.Components
+{
+    public class WorkoutFormDataComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(WorkoutDetailViewModel viewModel)
+        {
+            return await Task.FromResult<IViewComponentResult>(View(viewModel));
+        }
+    } 
+}
