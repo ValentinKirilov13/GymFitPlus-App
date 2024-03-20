bf1858e0920980fd95576a089d83e481dd7cce26
Valentin Kirilov <valkilmaar@gmail.com>
2024-03-20 03:21:20 +0200

    Edit workout viewModels

--- GymFitPlus.Core/ViewModels/WorkoutViewModels/WorkoutAllViewModel.cs
+++ GymFitPlus.Core/ViewModels/WorkoutViewModels/WorkoutAllViewModel.cs
@@ -1,15 +1,21 @@
-﻿namespace GymFitPlus.Core.ViewModels.WorkoutViewModels
+﻿using System.ComponentModel.DataAnnotations;
+using static GymFitPlus.Infrastructure.Constants.DataConstants.WorkoutConstants;
+using static GymFitPlus.Core.ErrorMessages.ErrorMessages;
+
+namespace GymFitPlus.Core.ViewModels.WorkoutViewModels
 {
     public class WorkoutAllViewModel
     {
         public int Id { get; set; }
 
         public Guid UserId { get; set; }
-  
+
         public string FitnessProgramName { get; set; } = string.Empty;
 
         public DateTime Date { get; set; }
 
+        [Display(Name = "Duration of workout")]
+        [Range(DurationMinValue, DurationMaxValue, ErrorMessage = DurationErrorMessages)]
         public int Duration { get; set; }
     }
 }
--- GymFitPlus.Core/ViewModels/WorkoutViewModels/WorkoutDetailViewModel.cs
+++ GymFitPlus.Core/ViewModels/WorkoutViewModels/WorkoutDetailViewModel.cs
@@ -1,15 +1,18 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using static GymFitPlus.Infrastructure.Constants.DataConstants.WorkoutConstants;
+using static GymFitPlus.Core.ErrorMessages.ErrorMessages;
 
 namespace GymFitPlus.Core.ViewModels.WorkoutViewModels
 {
     public class WorkoutDetailViewModel : WorkoutAllViewModel
     {
-        [Required]
+        [Required(ErrorMessage = RequiredErrorMessage)]
         public int FitnessProgramId { get; set; }
 
 
-        [MaxLength(NoteMaxLenght)]
+        [StringLength(NoteMaxLenght,
+            MinimumLength = NoteMinLenght,
+            ErrorMessage = LengthErrorMessage)]
         public string? Note { get; set; }
     }
 }
