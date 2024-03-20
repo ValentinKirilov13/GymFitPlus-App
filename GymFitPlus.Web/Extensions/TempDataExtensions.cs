022e6976f9eb61c41f44db88e65f86a6b8be4c6e
Valentin Kirilov <valkilmaar@gmail.com>
2024-03-20 03:20:48 +0200

    Add tempdata extensions

---
+++ GymFitPlus.Web/Extensions/TempDataExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace Microsoft.AspNetCore.Mvc.ViewFeatures
+{
+    public static class TempDataExtensions
+    {
+        public static void Put<T>(this ITempDataDictionary tempData, string key, T value) where T : class
+        {
+            tempData[key] = JsonSerializer.Serialize(value);
+        }
+
+        public static T? Get<T>(this ITempDataDictionary tempData, string key) where T : class
+        {          
+            tempData.TryGetValue(key, out object? obj);
+            return obj == null ? null : JsonSerializer.Deserialize<T>((string)obj);
+        }
+    }
+}
