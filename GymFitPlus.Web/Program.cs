8bf80fcc9dd31f551fe0ed87bd9bf1dbecc20bd6
Valentin Kirilov <valkilmaar@gmail.com>
2024-03-23 10:59:49 +0200

    Add antiforgery token and custom error page path

--- GymFitPlus.Web/Program.cs
+++ GymFitPlus.Web/Program.cs
@@ -1,4 +1,5 @@
 using GymFitPlus.Web.ModelBinders;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,17 +11,20 @@ builder.Services.AddApplicationAuthentication();
 builder.Services.AddControllersWithViews(opt =>
 {
     opt.ModelBinderProviders.Insert(0, new DateTimeModelBinderProvider());
+    opt.Filters.Add<AutoValidateAntiforgeryTokenAttribute>();
 });
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseMigrationsEndPoint();
 }
 else
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Home/Error/500");
+    app.UseStatusCodePagesWithReExecute("/Home/Error", "?statusCode={0}");
     app.UseHsts();
 }
 
