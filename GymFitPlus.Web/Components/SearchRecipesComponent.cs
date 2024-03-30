cc10e781647b2ab6be9b608242523566ef886288
Valentin Kirilov <valkilmaar@gmail.com>
2024-03-31 01:56:29 +0200

    Add viewComponent for Recipes

---
+++ GymFitPlus.Web/Components/SearchRecipesComponent.cs
@@ -0,0 +1,13 @@
+﻿using GymFitPlus.Core.ViewModels.RecipeViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymFitPlus.Web.Components
+{
+    public class SearchRecipesComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(AllRecipesQueryModel query)
+        {
+            return await Task.FromResult<IViewComponentResult>(View(query));
+        }
+    }
+}
