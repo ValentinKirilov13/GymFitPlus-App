1239f800071a9cb5b297571f0aee35be614d36b4
Valentin Kirilov <valkilmaar@gmail.com>
2024-04-02 21:38:54 +0300

    Configure model builder

--- GymFitPlus.Infrastructure/Data/ApplicationDbContext.cs
+++ GymFitPlus.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using GymFitPlus.Infrastructure.Data.Configuration;
 using GymFitPlus.Infrastructure.Data.Models;
+using GymFitPlus.Infrastructure.Data.SeedDatabase;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,22 +15,27 @@ namespace GymFitPlus.Infrastructure.Data
         public DbSet<Exercise> Exercises { get; set; } = null!;
         public DbSet<FitnessProgram> FitnessPrograms { get; set; } = null!;
         public DbSet<Workout> Workouts { get; set; } = null!;
-        public DbSet<UserSatistics> UserSatistics { get; set; } = null!;
+        public DbSet<UserStatistics> UserStatistics { get; set; } = null!;
         public DbSet<Recipe> Recipes { get; set; } = null!;
         public DbSet<UserRecipe> UsersRecipes { get; set; } = null!;
         public DbSet<FitnessProgramExercise> FitnessProgramsExercises { get; set; } = null!;
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            builder.ApplyConfiguration(new ApplicationUserConfiguration());
-            builder.ApplyConfiguration(new ApplicationRoleConfiguration());
-            builder.ApplyConfiguration(new FitnessProgramExerciseConfiguration());
-            builder.ApplyConfiguration(new ExerciseConfiguration());
-            builder.ApplyConfiguration(new WorkoutConfiguration());
-            builder.ApplyConfiguration(new FitnessProgramConfiguration());
-            builder.ApplyConfiguration(new UserSatisticsConfiguration());
-            builder.ApplyConfiguration(new RecipeConfiguration());
-            builder.ApplyConfiguration(new UserRecipeConfiguration());
+            SeedData dataForSeed = new SeedData();
+
+            builder.ApplyConfiguration(new ApplicationUserConfiguration(dataForSeed));
+            builder.ApplyConfiguration(new ApplicationRoleConfiguration(dataForSeed));
+            builder.ApplyConfiguration(new ApplicationUserRoleConfiguration(dataForSeed));
+
+            builder.ApplyConfiguration(new ExerciseConfiguration(dataForSeed));
+            builder.ApplyConfiguration(new RecipeConfiguration(dataForSeed));
+
+            builder.ApplyConfiguration(new FitnessProgramConfiguration(dataForSeed));
+            builder.ApplyConfiguration(new FitnessProgramExerciseConfiguration(dataForSeed));
+            builder.ApplyConfiguration(new UserRecipeConfiguration(dataForSeed));
+            builder.ApplyConfiguration(new UserStatisticsConfiguration(dataForSeed));
+            builder.ApplyConfiguration(new WorkoutConfiguration(dataForSeed));
 
             base.OnModelCreating(builder);
         }
